6e9811064cb28c33d725c26f68f26d9bd044fb52
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-12-14 20:51:25 +0530

    increased results display no

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -6,7 +6,7 @@ export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params:{
         part:'snippet',
-        maxResults:5,
+        maxResults:20,
         key:KEY,
     }
 })
